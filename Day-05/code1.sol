e2021f802825708dafd562eb89bcaf8f06543db4
anishsingh90 <anishraaz90@gmail.com>
2025-02-13 23:21:14 +0530

    string in soliidty

---
+++ Day-05/code1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Solidity{
+    string text;
+
+    function setText() public returns(string memory){
+        text = "Anish Singh";
+    }
+
+    function getText() public view returns(string memory){
+        return text;
+    }
+}
