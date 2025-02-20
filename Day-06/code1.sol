33a24e0d576c09f4bd1f7fe6c828526e50ad0ec0
anishsingh90 <anishraaz90@gmail.com>
2025-02-20 17:28:28 +0530

    strings in solidity

---
+++ Day-06/code1.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityLearn{
+    string text;
+
+    function setText() public returns(string memory){
+        text = "Anish";
+    }
+
+    function getText() public view returns(string memory){
+        return text;
+    }
+}
