b5e39e1f43154bd9e12f6100486575eade76d9b4
anishsingh90 <anishraaz90@gmail.com>
2025-03-04 09:07:48 +0530

    Simple Commit

---
+++ Day-08/code3.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract SolidityString{
+    string userInput;
+
+    function set(string memory finalValue) public {
+        userInput = finalValue;
+    }
+
+    function get() public view returns(string memory ){
+        return userInput;
+    }
+}
---
+++ Solidity Practice/lending.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Solidity{
+    struct lender{
+        address lenderAddress;
+        uint256 amount;
+        uint256 interestRate;
+    }
+}
