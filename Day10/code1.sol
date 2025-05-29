6a2bec5d70a6010022208620c409444e24314d5e
anishsingh90 <anishraaz90@gmail.com>
2025-05-29 10:09:02 +0530

    jd

--- Day10/code1.sol
+++ Day10/code1.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract 
+contract Solidity{
+    function add() public pure returns (uint){
+        uint num1 = 10;
+        uint num2 = 16;
+        uint sum = num1 + num2;
+        return sum;
+    }
+}
