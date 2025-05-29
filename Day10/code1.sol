69a87bdfa4bc9716a3440e23d336eeda9b5eea0b
anishsingh90 <anishraaz90@gmail.com>
2025-05-29 10:27:15 +0530

    function modifier

--- Day10/code1.sol
+++ Day10/code1.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract Solidity{
-    function add() public pure returns (uint){
-        uint num1 = 10;
-        uint num2 = 16;
-        uint sum = num1 + num2;
-        return sum;
+contract ModifierWithoutArg{
+    address admin;
+    struct employee{
+        uint emp_id;
+        string emp_name;
+        uint age;
     }
+
+    constructor() public {
+        admin = msg.sender;
+    }
+
+    modifier isAdmin{
+        require(admin == msg.sender);
+        _;
+    }
+
+    employee e;
+     function enterDetails(uint _empid, string memory _empname, uint _empage) public isAdmin{
+        e.emp_id = _empid;
+        e.emp_name = _empname;
+        e.age = _empage;
+     }
 }
