d2c01e58f282f9a9662e635a19a0d284ba802ab6
anishsingh90 <anishraaz90@gmail.com>
2025-06-17 10:55:28 +0530

    Array

--- Day-12/code1.sol
+++ Day-12/code1.sol
@@ -2,17 +2,8 @@
 pragma solidity ^0.5.0;
 
 contract LearningStrings{
-    string public text;
-
-    function setText() public {
-        text = 'hello';
-    }
-
-    function setTextByPassing(string memory message) public {
-        text = message;
-    }
-
-    function getText() view public returns(string memory){
-        return text;
+    function getLength(string memory s) public pure returns(uint256){
+        bytes memory b = bytes(s);
+        return b.length;
     }
 }
