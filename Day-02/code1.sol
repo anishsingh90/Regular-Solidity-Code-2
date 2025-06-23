400fa7c4f9299e22b3834d9c3939c5976ca3ae52
anishsingh90 <anishraaz90@gmail.com>
2025-06-23 10:20:20 +0530

    h

--- Day-02/code1.sol
+++ Day-02/code1.sol
@@ -1,11 +1,2 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
-
-contract SolidityTest{
-    function factorial(uint n) public pure returns(uint){
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        return n * factorial(n-1); 
-    }
-}
